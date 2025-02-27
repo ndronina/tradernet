8c11c49780f28a50388721a31467525a6c61bbd3
Anastasiia <ndronina42@gmail.com>
2025-02-27 10:26:05 -0800

    Minor fixes

--- app/src/main/java/com/ndronina/sample/tradernet/presentation/ui/TickersScreen.kt
+++ app/src/main/java/com/ndronina/sample/tradernet/presentation/ui/TickersScreen.kt
@@ -4,36 +4,29 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import com.ndronina.sample.tradernet.presentation.TickersViewModel
 import com.ndronina.sample.tradernet.presentation.model.UiState
 
 @Composable
 fun TickersScreen(viewModel: TickersViewModel) {
-    val uiState = remember { mutableStateOf(UiState()) }
-
-    LaunchedEffect(Unit) {
-        viewModel.state.collect { newState ->
-            uiState.value = newState
-        }
-    }
+    val uiState by viewModel.state.collectAsState(UiState())
 
     Scaffold { innerPadding ->
-        if (uiState.value.isLoading) {
+        if (uiState.isLoading) {
             LoadingState(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(innerPadding)
             )
-        } else if (!uiState.value.errorMessage.isNullOrEmpty()) {
+        } else if (!uiState.errorMessage.isNullOrEmpty()) {
             ErrorState(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(innerPadding),
-                errorMessage = requireNotNull(uiState.value.errorMessage),
+                errorMessage = requireNotNull(uiState.errorMessage),
                 onRetry = viewModel::onRetry
             )
         } else {
@@ -41,7 +34,7 @@ fun TickersScreen(viewModel: TickersViewModel) {
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(innerPadding),
-                data = uiState.value.data
+                data = uiState.data
             )
         }
     }
